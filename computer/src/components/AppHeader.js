afc46b01b6205a58ef18f3ac9a8d97f7f05d858c
talhaHavadar <talhahavadar@hotmail.com>
2019-07-07 14:22:47 +0300

    Update main window by adding logo.

--- computer/src/components/AppHeader.js
+++ computer/src/components/AppHeader.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './AppHeader.css';
+import { ReactComponent as Logo } from '../logo.svg';
 
 class AppHeader extends React.Component {
     render() {
@@ -7,7 +8,7 @@ class AppHeader extends React.Component {
             <header className="app-header">
                 <div className="content">
                     <div className="hamburger">
-                        |||
+                        <Logo style={{ width:"100%", height:"100%"}}/>
                     </div>
                     <div className="title">
                         Discovered Devices
--- computer/src/components/DeviceListItem.js
+++ computer/src/components/DeviceListItem.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import './DeviceListItem.css';
+import { ReactComponent as Logo } from '../logo.svg';
 
 class DeviceListItem extends React.Component {
     render() {
         const {name, ip_addr} = this.props;
         return (
             <li className="device-list-item gradient-border">
-                <div className="status">O</div>
+                <div className="status">
+                    <Logo style={{ width:"100%", height:"100%"}}/>
+                </div>
                 <div className="name">{name}</div>
                 <div className="separator" />
                 <div className="ip">{ip_addr}</div>

69b8ad047d9b2924e8da57d3e2deabd1f20d698b
talhaHavadar <havadartalha@gmail.com>
2019-07-23 16:23:54 +0300

    Add device vuex store.

--- application/src/store/index.js
+++ application/src/store/index.js
@@ -1,7 +1,7 @@
 import Vue from "vue";
 import Vuex from "vuex";
 
-// import example from './module-example'
+import device from "./device";
 
 Vue.use(Vuex);
 
@@ -13,13 +13,27 @@ Vue.use(Vuex);
 export default function(/* { ssrContext } */) {
   const Store = new Vuex.Store({
     modules: {
-      // example
+      device
     },
 
     // enable strict mode (adds overhead!)
     // for dev mode only
     strict: process.env.DEV
   });
+
+  /*
+    if we want some HMR magic for it, we handle
+    the hot update like below. Notice we guard this
+    code with "process.env.DEV" -- so this doesn't
+    get into our production build (and it shouldn't).
+  */
+
+  if (process.env.DEV && module.hot) {
+    module.hot.accept(["./device"], () => {
+      const newShowcase = require("./device").default;
+      Store.hotUpdate({ modules: { showcase: newShowcase } });
+    });
+  }
 
   return Store;
 }

02f4a05bfc4871ebc37cbfaccbb5eed4fcb4e707
Talha Can Havadar <havadartalha@gmail.com>
2025-03-30 12:48:13 +0200

    create initial cli api

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+use clap::{Parser, Subcommand};
+use log::{info, warn};
+
+#[derive(Parser, Debug)]
+#[command(name = "copycat")]
+#[command(bin_name = "copycat")]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+    #[arg()]
+    device_name: Option<String>,
+}
+
+#[derive(Subcommand, Debug)]
+enum Commands {
+    List {},
+    Enable {},
+    Disable {},
+}
+
 fn main() {
+    env_logger::init();
+    info!("Hello logging!");
+    let args = Cli::parse();
+
+    println!("cli: {:?}", args);
     println!("Hello, world!");
 }
